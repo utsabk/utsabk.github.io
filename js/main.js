fa253dce86ec7386161bf2f4f54125f4a4484e3f
utsabk <kharelutsab@gmail.com>
2020-04-17 22:54:27 +0300

    New Changes Includes: - IndexedDB   * 'Output' for sending directly to server(if online)   * 'Input' incomplete - Background Sync with Service Worker  *

--- js/main.js
+++ js/main.js
@@ -3,30 +3,63 @@
 window.addEventListener('load', async () => {
 
   const ul = document.querySelector('ul');
+  const rfrsh = document.querySelector('#refresh');
   const form = document.querySelector('form');
-  const message = form.elements.message;
+  const username = 'utsabk';
+  const greeting = form.elements.message;
 
   if ('serviceWorker' in navigator) {
-    await navigator.serviceWorker.register('../sw.js');
+    try {
 
-    const data = [];
+      await navigator.serviceWorker.register('./sw.js');
 
-    form.addEventListener('submit', () => {
+      const registration = await navigator.serviceWorker.ready;
 
-      event.preventDefault();
+      if ('sync' in registration) {
 
-      data.push(message.value);
+        form.addEventListener('submit', async(event) => {
 
-      console.log('inside event listner',data);
-      ul.innerHTML = '';
-      data.forEach(item =>
-          ul.innerHTML += `<li>${item}</li>`,
-      );
+          event.preventDefault();
 
+          const message = {
+            username,
+            greeting: greeting.value,
+          };
 
-      message.value = '';
+          try {
+            saveData('outbox', message);
+            await registration.sync.register('send-message')
+          }catch(e){
+            console.log('error while saving data',e.message)
+          }
+        });
 
-    });
+      }
 
+    } catch (e) {
+      console.log('service worker error:-', e.message);
+    }
   }
+
+  const init = async () => {
+    const data = [];
+    try {
+      const greetings = await getGreetingsByUser(username);
+      for (const message of greetings) {
+        data.push(message);
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    ul.innerHTML = '';
+    data.forEach(item => {
+      ul.innerHTML += `<li>${item.username}: ${item.greeting}</li>`;
+    });
+  };
+
+  init();
+
+  rfrsh.addEventListener('click', init);
+
 });
--- sw.js
+++ sw.js
@@ -1,31 +1,66 @@
+self.importScripts('./js/fetchGQL.js');
+self.importScripts('./js/idb.js');
+
 const cacheName = 'hello-pwa';
-const filesToCache = ['./', './index.html', './css/style.css', './js/main.js', './images/hello.png'];
+const filesToCache = [
+  './',
+  './index.html',
+  './favicon.ico',
+  './css/style.css',
+  './js/main.js',
+  './js/idb.js',
+  './images/hello.png'];
 
 //* Start the service worker and cache all of the app's content */
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    (async () => {
-      try {
-        const cache = await caches.open(cacheName);
-        // console.log(cache);
-        return cache.addAll(filesToCache);
-      } catch (e) {
-        console.log('after install', e.message);
-      }
-    })()
+      (async () => {
+        try {
+          const cache = await caches.open(cacheName);
+          // console.log(cache);
+          return cache.addAll(filesToCache);
+        } catch (e) {
+          console.log('after install', e.message);
+        }
+      })(),
   );
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    (async () => {
-      try {
-        const response = await caches.match(e.request);
-        return response || fetch(e.request);
-      } catch (e) {
-        console.log('after install', e.message);
-      }
-    })()
+      (async () => {
+        try {
+          const response = await caches.match(e.request);
+          return response || fetch(e.request);
+        } catch (e) {
+          console.log('after install', e.message);
+        }
+      })(),
   );
 });
+
+self.addEventListener('sync', (event) => {
+  if (event.tag == 'send-message') {
+    event.waitUntil(sendToServer());
+  }
+});
+
+const sendToServer = async () => {
+      try {
+        const outbox = await loadData('outbox');
+        console.log('outbox', outbox);
+
+        const sentMessages = await Promise.all(outbox.map(async (message) =>
+            await saveGreeting(message)));
+
+        console.log('SentMessages', sentMessages);
+
+        clearData('outbox');
+
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+;
+
